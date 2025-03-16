d479a5181aaa425d53d1846ee4feb534a4c652e9
Supreeth S Karan <45706852+SupreethSudhakaranMenon@users.noreply.github.com>
2025-03-16 17:04:12 +0530

    Fix GitHub Pages configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,9 @@ module.exports = {
     title: 'Ayushi Agrawal',
     description:
       'Ayushi Agrawal is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://ayushiagrawal.com', // No trailing slash allowed!
+    siteUrl: 'https://supreethsudhakaranmenon.github.io', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@ayushiagrawal', // Need to modify this
+    twitterUsername: '@ayushiagrawal',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
