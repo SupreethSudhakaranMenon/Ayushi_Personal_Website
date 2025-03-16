19b0a5a9b73dfffcf115385550eb2f38e212fbf9
Supreeth S Karan <45706852+SupreethSudhakaranMenon@users.noreply.github.com>
2025-03-16 17:11:06 +0530

    Update deployment configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,8 @@ module.exports = {
     title: 'Ayushi Agrawal',
     description:
       'Ayushi Agrawal is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://supreethsudhakaranmenon.github.io', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
+    siteUrl: 'https://supreethsudhakaranmenon.github.io/Ayushi_Personal_Website',
+    image: '/og.png',
     twitterUsername: '@ayushiagrawal',
   },
   plugins: [
